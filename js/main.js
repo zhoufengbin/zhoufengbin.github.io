c7391ef1a879ba9df829b8aa310d8548f1700c5a
zhoufengbin <zfb2264@163.com>
2021-12-30 21:38:48 +0800

    Site updated: 2021-12-30 21:38:47

--- js/main.js
+++ js/main.js
@@ -1,371 +1,241 @@
-$(document).ready(function () {
-  hljs.initHighlightingOnLoad();
-  clickTreeDirectory();
-  serachTree();
-  pjaxLoad();
-  showArticleIndex();
-  switchTreeOrIndex();
-  scrollToTop();
-  pageScroll();
-  wrapImageWithFancyBox();
-});
-
-// 页面滚动
-function pageScroll() {
-  var start_hight = 0;
-  $(window).on('scroll', function () {
-    var end_hight = $(window).scrollTop();
-    var distance = end_hight - start_hight;
-    start_hight = end_hight;
-    var $header = $('#header');
-    if (distance > 0 && end_hight > 50) {
-      $header.hide();
-    } else if (distance < 0) {
-      $header.show();
-    } else {
-      return false;
-    }
-  })
-}
-
-// 回到顶部
-function scrollToTop() {
-  $("#totop-toggle").on("click", function (e) {
-    $("html").animate({scrollTop: 0}, 200);
-  });
-}
-
-// 侧面目录
-function switchTreeOrIndex() {
-  $('#sidebar-toggle').on('click', function () {
-    if ($('#sidebar').hasClass('on')) {
-      scrollOff();
-    } else {
-      scrollOn();
-    }
-  });
-  $('body').click(function (e) {
-    if (window.matchMedia("(max-width: 1100px)").matches) {
-      var target = $(e.target);
-      if (!target.is('#sidebar *')) {
-        if ($('#sidebar').hasClass('on')) {
-          scrollOff();
-        }
-      }
-    }
-  });
-  if (window.matchMedia("(min-width: 1100px)").matches) {
-    scrollOn();
-  } else {
-    scrollOff();
-  }
-  ;
-}
-
-//生成文章目录
-function showArticleIndex() {
-  $(".article-toc").empty();
-  $(".article-toc").hide();
-  $(".article-toc.active-toc").removeClass("active-toc");
-  $("#tree .active").next().addClass('active-toc');
-
-  var labelList = $("#article-content").children();
-  var content = "<ul>";
-  var max_level = 4;
-  for (var i = 0; i < labelList.length; i++) {
-    var level = 5;
-    if ($(labelList[i]).is("h1")) {
-      level = 1;
-    } else if ($(labelList[i]).is("h2")) {
-      level = 2;
-    } else if ($(labelList[i]).is("h3")) {
-      level = 3;
-    } else if ($(labelList[i]).is("h4")) {
-      level = 4;
-    }
-    if (level < max_level) {
-      max_level = level;
-    }
-  }
-  for (var i = 0; i < labelList.length; i++) {
-    var level = 0;
-    if ($(labelList[i]).is("h1")) {
-      level = 1 - max_level + 1;
-    } else if ($(labelList[i]).is("h2")) {
-      level = 2 - max_level + 1;
-    } else if ($(labelList[i]).is("h3")) {
-      level = 3 - max_level + 1;
-    } else if ($(labelList[i]).is("h4")) {
-      level = 4 - max_level + 1;
-    }
-    if (level != 0) {
-      $(labelList[i]).before(
-          '<span class="anchor" id="_label' + i + '"></span>');
-      content += '<li class="level_' + level
-          + '"><i class="fa fa-circle" aria-hidden="true"></i><a href="#_label'
-          + i + '"> ' + $(labelList[i]).text() + '</a></li>';
-    }
-  }
-  content += "</ul>"
-
-  $(".article-toc.active-toc").append(content);
-
-  if (null != $(".article-toc a") && 0 != $(".article-toc a").length) {
-
-    // 点击目录索引链接，动画跳转过去，不是默认闪现过去
-    $(".article-toc a").on("click", function (e) {
-      e.preventDefault();
-      // 获取当前点击的 a 标签，并前触发滚动动画往对应的位置
-      var target = $(this.hash);
-      $("body, html").animate(
-          {'scrollTop': target.offset().top},
-          500
-      );
-    });
-
-    // 监听浏览器滚动条，当浏览过的标签，给他上色。
-    $(window).on("scroll", function (e) {
-      var anchorList = $(".anchor");
-      anchorList.each(function () {
-        var tocLink = $('.article-toc a[href="#' + $(this).attr("id") + '"]');
-        var anchorTop = $(this).offset().top;
-        var windowTop = $(window).scrollTop();
-        if (anchorTop <= windowTop + 100) {
-          tocLink.addClass("read");
-        } else {
-          tocLink.removeClass("read");
-        }
-      });
-    });
-  }
-  $(".article-toc.active-toc").show();
-  $(".article-toc.active-toc").children().show();
-}
-
-function pjaxLoad() {
-  $(document).pjax('#menu a', '#content',
-      {fragment: '#content', timeout: 8000});
-  $(document).pjax('#tree a', '#content',
-      {fragment: '#content', timeout: 8000});
-  $(document).pjax('#index a', '#content',
-      {fragment: '#content', timeout: 8000});
-  $(document).on({
-    "pjax:complete": function (e) {
-      $("pre code").each(function (i, block) {
-        hljs.highlightBlock(block);
-      });
-      // 添加 active
-      $("#tree .active").removeClass("active");
-      var title = $("#article-title").text().trim();
-      if (title.length) {
-        var searchResult = $("#tree li.file").find(
-            "a:contains('" + title + "')");
-        if (searchResult.length) {
-          $(".fa-minus-square-o").removeClass("fa-minus-square-o").addClass(
-              "fa-plus-square-o");
-          $("#tree ul").css("display", "none");
-          if (searchResult.length > 1) {
-            var categorie = $("#article-categories span:last a").html();
-            if (typeof categorie != "undefined") {
-              categorie = categorie.trim();
-              searchResult = $("#tree li.directory a:contains('" + categorie
-                  + "')").siblings().find("a:contains('" + title + "')");
+(function (w, d) {
+    var body = $('body, html'),
+        toc = $("#post-toc"),
+        headerMenu = $("#header-menu"),
+        backTop = $("#sidebar-top"),
+        search = $('#sidebar-search'),
+        searchWrap = $('.search-wrap'),
+        tags = $("#sidebar-menu-box-tags"),
+        mobileTags = $("#mobile-header-container-tags"),
+        categories = $("#sidebar-menu-box-categories"),
+        sideMenuBox = $("#sidebar-menu-box"),
+        mobileHeaderMenu = $("#mobile-header-menu-nav"),
+        _mobileHeaderMenuLocked = false,
+        sideMenuBoxIsOpen = true,
+        clientHeight = d.documentElement.clientHeight; //获取可视区的高度
+    var Blog = {
+        showHeaderMenu: function (scrollTop) {
+            if (scrollTop > clientHeight * 0.1) {
+                headerMenu.removeClass("slide-down");
+                headerMenu.addClass("slide-up");
+            } else {
+                headerMenu.removeClass("slide-up");
+                headerMenu.addClass("slide-down");
             }
-          }
-          searchResult[0].parentNode.classList.add("active");
-          showActiveTree($("#tree .active"), true)
+        },
+        showBackTop: function (scrollTop) {
+            backTop.css('display', (scrollTop > clientHeight) ? "block" : "none");
+        },
+        setTags: function (tags) {
+            var labels = tags.find("a");
+            labels.css({"font-size" : "15px"});
+            for(var i = 0, len = labels.length; i < len; i++){
+                var num = labels.eq(i).html().length % 5 +1;
+                labels[i].className = "";
+                labels.eq(i).addClass("color"+num);
+            }
+        },
+        setCategories: function () {
+            var labels = categories.find("a");
+            labels.css({"font-size" : "15px"});
+            for(var i = 0, len = labels.length; i < len; i++){
+                var num = labels.eq(i).html().length % 5 +1;
+                labels[i].className = "";
+                labels.eq(i).addClass("color"+num);
+            }
+        },
+        showSidebarBox: function (status) {
+            if (status) {
+                sideMenuBox.animate({
+                    height:'162px',
+                    opacity:'1'
+                }, 300);
+            } else {
+                sideMenuBox.animate({
+                    height:'0px',
+                    opacity:'0'
+                }, 300);
+            }
+        },
+        showToc: function (scrollTop) {
+            if (scrollTop / clientHeight >= 0.4) {
+                toc.removeClass("post-toc-top");
+                toc.addClass("post-toc-not-top");
+            } else {
+                toc.removeClass("post-toc-not-top");
+                toc.addClass("post-toc-top");
+            }
+        },
+        showMobileHeaderMenu: function (status) {
+            if (_mobileHeaderMenuLocked) {
+                return false;
+            }
+            if (status) {
+                mobileHeaderMenu.addClass("mobile-header-menu-nav-in");
+            } else {
+                mobileHeaderMenu.removeClass("mobile-header-menu-nav-in")
+            }
+        },
+        hideMask: function (target) {
+            var mask = $('.mask');
+            mask.removeClass('in');
+            if (target) {
+                target.removeClass('in')
+            }
+        },
+        share: function () {
+            var shareSub = $('#share-sub');
+            if (shareSub) {
+                var shareList = $('#share-list'),
+                    wxFab = $('#wxFab'),
+                    close = $('#wxShare-close'),
+                    mask = $('.mask');
+                shareSub.click(function () {
+                    if (shareList.hasClass('in')) {
+                        shareList.removeClass('in');
+                    } else {
+                        shareList.addClass('in');
+                    }
+                });
+                wxFab.click(function () {
+                    var wxShare = $('#wxShare');
+                    wxShare.addClass('in ready');
+                    mask.addClass('in');
+                });
+                close.click(function () {
+                    Blog.hideMask($('#wxShare'));
+                });
+                mask.click(function () {
+                    Blog.hideMask($('#wxShare'));
+                });
+            }
+        },
+        reward: function () {
+            var reward = $('#reward'),
+                close = $('#reward-close'),
+                rewardCode = $('#rewardCode'),
+                rewardCheck = $('.reward-select-item'),
+                mask = $('.mask');
+            if (reward) {
+                var rewardBtn = $('#rewardBtn');
+                rewardBtn.click(function () {
+                    reward.addClass('in ready');
+                    mask.addClass('in');
+                });
+                rewardCheck.click(function () {
+                    $(this).addClass('checked').siblings(rewardCheck).removeClass('checked');
+                    rewardCode.attr('src', $(this).attr('data-id') === 'wechat' ? this.dataset.wechat : this.dataset.alipay);
+                });
+                close.click(function () {
+                    Blog.hideMask(reward);
+                });
+                mask.click(function () {
+                    Blog.hideMask(reward);
+                });
+            }
+        },
+    };
+
+    //初始化搜索数据
+    initSearch();
+    //搜索点击事件
+    search.click(function () {
+        searchWrap.css('top','50%');
+        searchWrap.css('marginTop','-80px');
+        searchWrap.css('opacity','1');
+    });
+    $('.search-close').click(function(){
+        searchWrap.css('top','0');
+        searchWrap.css('opacity','0');
+        $('#search-container').removeClass('search-container-show');
+    });
+
+    //tags | 标签
+    Blog.setTags(tags);//pc
+    Blog.setTags(mobileTags);//mobile
+    //categories | 类别
+    Blog.setCategories();
+    //类别展示
+    $("#sidebar-category").click(function (e) {
+        tags.css('display', 'none');
+        categories.css('display', 'block');
+        e.stopPropagation();
+        if (sideMenuBoxIsOpen) {
+            Blog.showSidebarBox(true);
+            sideMenuBoxIsOpen = false;
+        } else {
+            Blog.showSidebarBox(false);
+            sideMenuBoxIsOpen = true;
         }
-        showArticleIndex();
-      }
-      wrapImageWithFancyBox();
-    }
-  });
-}
+    });
+    //标签展示
+    $("#sidebar-tag").click(function (e) {
+        tags.css('display', 'block');
+        categories.css('display', 'none');
+        e.stopPropagation();
+        if (sideMenuBoxIsOpen) {
+            Blog.showSidebarBox(true);
+            sideMenuBoxIsOpen = false;
+        } else {
+            Blog.showSidebarBox(false);
+            sideMenuBoxIsOpen = true;
+        }
+    });
+    //点击菜单区域不能关闭菜单
+    sideMenuBox.click(function (e) {
+        e.stopPropagation();
+        if (sideMenuBoxIsOpen) {
+            return false;
+        }
+    });
+    //点击close按钮关闭菜单
+    $(".sidebar-menu-box-close").click(function() {
+        Blog.showSidebarBox(false);
+        sideMenuBoxIsOpen = true;
+    });
 
-// 搜索框输入事件
-function serachTree() {
-  // 解决搜索大小写问题
-  jQuery.expr[':'].contains = function (a, i, m) {
-    return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
-  };
+    //回到顶部点击事件
+    backTop.click(function () {
+        body.animate({
+            scrollTop: 0
+        }, 500);
+    });
 
-  $("#search-input").on("input", function (e) {
-    e.preventDefault();
+    //获取滚动事件
+    d.addEventListener('scroll', function () {
+        var scrollTop = d.documentElement.scrollTop || d.body.scrollTop;
+        Blog.showHeaderMenu(scrollTop);
+        Blog.showBackTop(scrollTop);
+        Blog.showToc(scrollTop);
+    }, false);
+    
+    //Mobile Menu
+    $(".mobile-header-menu-button").click(function () {
+        if (_mobileHeaderMenuLocked) {
+            return false;
+        }
+        Blog.showMobileHeaderMenu(true);
 
-    // 获取 inpiut 输入框的内容
-    var inputContent = e.currentTarget.value;
+        _mobileHeaderMenuLocked = true;
 
-    // 没值就收起父目录，但是得把 active 的父目录都展开
-    if (inputContent.length === 0) {
-      $(".fa-minus-square-o").removeClass("fa-minus-square-o").addClass(
-          "fa-plus-square-o");
-      $("#tree ul").css("display", "none");
-      if ($("#tree .active").length) {
-        showActiveTree($("#tree .active"), true);
-      } else {
-        $("#tree").children().css("display", "block");
-      }
-    }
-    // 有值就搜索，并且展开父目录
-    else {
-      $(".fa-plus-square-o").removeClass("fa-plus-square-o").addClass(
-          "fa-minus-square-o");
-      $("#tree ul").css("display", "none");
-      var searchResult = $("#tree li").find(
-          "a:contains('" + inputContent + "')");
-      if (searchResult.length) {
-        showActiveTree(searchResult.parent(), false)
-      }
-    }
-  });
-
-  $("#search-input").on("keyup", function (e) {
-    e.preventDefault();
-    if (event.keyCode == 13) {
-      var inputContent = e.currentTarget.value;
-
-      if (inputContent.length === 0) {
-      } else {
-        window.open(searchEngine + inputContent + "%20site:" + homeHost,
-            "_blank");
-      }
-    }
-  });
-}
-
-// 点击目录事件
-function clickTreeDirectory() {
-  // 判断有 active 的话，就递归循环把它的父目录打开
-  var treeActive = $("#tree .active");
-  if (treeActive.length) {
-    showActiveTree(treeActive, true);
-  }
-
-  // 点击目录，就触发折叠动画效果
-  $(document).on("click", "#tree a[class='directory']", function (e) {
-    // 用来清空所有绑定的其他事件
-    e.preventDefault();
-
-    var icon = $(this).children(".fa");
-    var iconIsOpen = icon.hasClass("fa-minus-square-o");
-    var subTree = $(this).siblings("ul");
-
-    icon.removeClass("fa-minus-square-o").removeClass("fa-plus-square-o");
-
-    if (iconIsOpen) {
-      if (typeof subTree != "undefined") {
-        subTree.slideUp({duration: 100});
-      }
-      icon.addClass("fa-plus-square-o");
-    } else {
-      if (typeof subTree != "undefined") {
-        subTree.slideDown({duration: 100});
-      }
-      icon.addClass("fa-minus-square-o");
-    }
-  });
-}
-
-// 循环递归展开父节点
-function showActiveTree(jqNode, isSiblings) {
-  if (jqNode.attr("id") === "tree") {
-    return;
-  }
-  if (jqNode.is("ul")) {
-    jqNode.css("display", "block");
-
-    // 这个 isSiblings 是给搜索用的
-    // true 就显示开同级兄弟节点
-    // false 就是给搜索用的，值需要展示它自己就好了，不展示兄弟节点
-    if (isSiblings) {
-      jqNode.siblings().css("display", "block");
-      jqNode.siblings("a").css("display", "inline");
-      jqNode.siblings("a").find(".fa-plus-square-o").removeClass(
-          "fa-plus-square-o").addClass("fa-minus-square-o");
-    }
-  }
-  jqNode.each(function () {
-    showActiveTree($(this).parent(), isSiblings);
-  });
-}
-
-function scrollOn() {
-  var $sidebar = $('#sidebar'),
-      $content = $('#content'),
-      $header = $('#header'),
-      $footer = $('#footer'),
-      $togglei = $('#sidebar-toggle i');
-
-  $togglei.addClass('fa-close');
-  $togglei.removeClass('fa-arrow-right');
-  $sidebar.addClass('on');
-  $sidebar.removeClass('off');
-
-  if (window.matchMedia("(min-width: 1100px)").matches) {
-    $content.addClass('content-on');
-    $content.removeClass('content-off');
-    $header.addClass('header-on');
-    $header.removeClass('off');
-    $footer.addClass('header-on');
-    $footer.removeClass('off');
-  }
-}
-
-function scrollOff() {
-  var $sidebar = $('#sidebar'),
-      $content = $('#content'),
-      $header = $('#header'),
-      $footer = $('#footer'),
-      $togglei = $('#sidebar-toggle i');
-
-  $togglei.addClass('fa-arrow-right');
-  $togglei.removeClass('fa-close');
-  $sidebar.addClass('off');
-  $sidebar.removeClass('on');
-
-  $content.addClass('off');
-  $content.removeClass('content-on');
-  $header.addClass('off');
-  $header.removeClass('header-on');
-  $footer.addClass('off');
-  $footer.removeClass('header-on');
-}
-
-/**
- * Wrap images with fancybox support.
- */
-function wrapImageWithFancyBox() {
-  $('img').not('#header img').each(function () {
-    var $image = $(this);
-    var imageCaption = $image.attr('alt');
-    var $imageWrapLink = $image.parent('a');
-
-    if ($imageWrapLink.length < 1) {
-      var src = this.getAttribute('src');
-      var idx = src.lastIndexOf('?');
-      if (idx != -1) {
-        src = src.substring(0, idx);
-      }
-      $imageWrapLink = $image.wrap('<a href="' + src + '"></a>').parent('a');
+        window.setTimeout(function() {
+            _mobileHeaderMenuLocked = false;
+        }, 350);
+    });
+    
+    //Share
+    if (w.mihoConfig.share) {
+        Blog.share();
     }
 
-    $imageWrapLink.attr('data-fancybox', 'images');
-    if (imageCaption) {
-      $imageWrapLink.attr('data-caption', imageCaption);
+    //Reward
+    if (w.mihoConfig.reward === 1 || w.mihoConfig.reward === 2) {
+        Blog.reward();
     }
-
-  });
-
-  $('[data-fancybox="images"]').fancybox({
-    buttons: [
-      'slideShow',
-      'thumbs',
-      'zoom',
-      'fullScreen',
-      'close'
-    ],
-    thumbs: {
-      autoStart: false
-    }
-  });
-}
+    //body
+    body.click(function () {
+        Blog.showSidebarBox(false);
+        sideMenuBoxIsOpen = true;
+        Blog.showMobileHeaderMenu(false);
+    });
+})(window, document);
